84f289d3f2101da204f0d8a55c8fd8266d005ab7
Rafael Cronemberger <rafael@berger.com.br>
2023-07-29 20:18:43 -0300

    Initial commit

---
+++ src/Berger.Extensions.Configuration/Helpers/AppSettingConfiguration.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace Berger.Extensions.Configuration
+{
+    public static class AppSettingConfiguration
+    {
+        public static IConfigurationBuilder ConfigureAppSettings(this IConfiguration configuration, string path, string environment)
+        {
+            var builder = new ConfigurationBuilder()
+                    .SetBasePath(path)
+                    .AddJsonFile("appsettings.json", false, true)
+                    .AddJsonFile($"appsettings.{environment}.json", true);
+
+            builder.AddEnvironmentVariables();
+
+            return builder;
+        }
+        public static IConfigurationBuilder ConfigureAppSettings(this IConfiguration configuration)
+        {
+            var builder = new ConfigurationBuilder().AddJsonFile("appsettings.json", false, true);
+
+            builder.AddEnvironmentVariables();
+
+            return builder;
+        }
+        public static T Teste<T>(this IConfiguration configuration, string key)
+        {
+            return configuration.GetSection(key).Get<T>();
+        }
+    }
+}
---
+++ src/Berger.Extensions.Configuration/Helpers/ConfigurationHelper.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace Berger.Extensions.Configuration
+{
+    public static class ConfigurationHelper
+    {
+        #region Properties
+        public static IConfiguration _configuration;
+        #endregion
+
+        #region Constructors
+        public static void Initialize(IConfiguration Configuration)
+        {
+            _configuration = Configuration;
+        }
+        #endregion
+
+        #region Methods
+        public static IConfigurationBuilder ConfigureAppSettings(this IConfiguration configuration)
+        {
+            var builder = new ConfigurationBuilder().AddJsonFile("appsettings.json", false, true);
+
+            return builder;
+        }
+        #endregion
+    }
+}
