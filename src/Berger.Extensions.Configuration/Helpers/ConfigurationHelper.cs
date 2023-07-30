786c592a528ef3fcfaf994a6c8061852e161584e
Rafael Cronemberger <rafael@berger.com.br>
2023-07-30 01:45:43 -0300

    Readme updates

---
+++ src/Berger.Extensions.Configuration/Helpers/ConfigurationHelper.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace Berger.Extensions.Configuration
+{
+    public static class ConfigurationHelper
+    {
+        #region Methods
+        public static IConfigurationBuilder ConfigureBuilder(this IConfiguration configuration, string path, string environment)
+        {
+            var builder = new BaseConfiguration();
+
+            return builder
+                .Set(path, environment)
+                .AddEnvironmentVariables();
+        }
+        public static IConfigurationBuilder ConfigureBuilder(this IConfigurationBuilder configuration, string path, string environment)
+        {
+            var builder = new BaseConfiguration();
+
+            return builder.Set(path, environment);
+        }
+        public static IConfigurationBuilder ConfigureBuilder(this IConfiguration configuration)
+        {
+            var builder = new BaseConfiguration();
+
+            return builder
+                .Set()
+                .AddEnvironmentVariables();
+        }
+
+        public static IConfigurationBuilder ConfigureBuilder(this IConfigurationBuilder configuration)
+        {
+            var builder = new BaseConfiguration();
+
+            return builder.Set();
+        }
+        public static IConfigurationRoot Build(this IConfiguration configuration, string path, string environment)
+        {
+            var builder = new BaseConfiguration();
+
+            return builder.Set(path, environment).Build();
+        }
+        public static IConfigurationRoot Build(this IConfiguration configuration, string directory = "")
+        {
+            var builder = new BaseConfiguration();
+
+            return builder.Set(directory).Build();
+        }
+        public static T Get<T>(this IConfiguration configuration, string key)
+        {
+            return configuration.GetSection(key).Get<T>();
+        }
+        #endregion
+    }
+}
